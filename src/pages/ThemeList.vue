bce7ecc61d4217d3044f25ca60711cdcb6f863bb
zhangguanyu02 <zhangguanyu02@meituan.com>
2017-05-21 20:26:49 +0800

    init

---
+++ src/pages/ThemeList.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="themeList">
+		<ArticleList :id="id"></ArticleList>
+	</div>
+</template>
+<script>
+	import ArticleList from '../components/AtricleList.vue';
+	export default {
+		name:"themeList",
+		data(){
+			return {
+
+			}
+		},
+		computed:{
+			id(){
+				return this.$route.query.id;
+			}
+		},
+		components:{
+			ArticleList
+		}
+	}
+</script>
